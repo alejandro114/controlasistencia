2e628d5781ba0def1bb5b3b180ab2e01a9e61604
JuanEracleo <juan@llika.net>
2015-04-29 18:50:05 -0500

    He eliminado db.sqlite, las clases he cambiado a mayuscula, he ignorado dbs

--- tablas/models.py
+++ tablas/models.py
@@ -1,28 +1,21 @@
 from django.db import models
-
-# Create your models here.
-
-class trabajadores(models.Model):
+class Trabajadores(models.Model):
     nombres=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
     telefono=models.CharField(max_length=10)
     correo=models.CharField(max_length=30)
     def __unicode__(self):
         return self.nombres
-
-class proyectos(models.Model):
-	cod=models.CharField(max_length=30)
-	detalles_proyecto=models.CharField(max_length=100)
-
-class controlasistencia(models.Model):
-	dias=models.CharField(max_length=30)
-	horas=models.CharField(max_length=30)
-	nombres=models.ForeignKey(trabajadores)
-
-
+class Proyectos(models.Model):
+    cod=models.CharField(max_length=30)
+    detalles_proyecto=models.CharField(max_length=100)
+class Controlasistencia(models.Model):
+    dias=models.CharField(max_length=30)
+    horas=models.CharField(max_length=30)
+    nombres=models.ForeignKey(Trabajadores)
 class TareasEncargadas(models.Model):
-	cod=models.ForeignKey(proyectos)
-	nro_encargados=models.CharField(max_length=02)#falta poner codigo de nuero
-	nombres	= models.ForeignKey(trabajadores)
-	fecha_inicio = models.DateField()
-	fecha_fin = models.DateField()
+    cod=models.ForeignKey(proyectos)
+    nro_encargados=models.CharField(max_length=02)#falta poner codigo de nuero
+    nombres	= models.ForeignKey(Trabajadores)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
